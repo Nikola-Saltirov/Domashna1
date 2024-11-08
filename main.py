ec805fa16d035d971a7f110648e5bf587f34ce41
Никола Салтиров <nikola.saltirov04@gmail.com>
2024-11-08 18:06:16 +0100

    need bugfix

--- main.py
+++ main.py
@@ -21,112 +21,104 @@ from bs4 import BeautifulSoup as bs, BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 
 #NAMES
-def filter1(driver):
+def filter1(url):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    driver=webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+    WebDriverWait(driver, 100).until(EC.presence_of_all_elements_located((By.ID, 'Code')))
     names = driver.find_elements(By.TAG_NAME, 'option')
     names = [str(n.text) for n in names]
-    dates=[]
     names2=[]
     for n in names:
         if any(char.isdigit() for char in n):
             continue
         else:
             names2.append(n)
-            dates.append(None)
-    directory_path = Path(f"stocks/files")
-    directory_path.mkdir(parents=True, exist_ok=True)
-
     print(len(names2))
-    print(len(dates))
     df=pd.DataFrame({
     'Names': names2,
-    'Last_date': dates
     })
     df.to_csv('stocks/names.csv',index=False)
+    filter2(url)
 
 #CHECK 10 YEARS IF EXISTS AND IF NEED UPDATE
-def filter2():
+def filter2(url):
     df=pd.read_csv('stocks/names.csv')
     names=df.Names
-    i=0
+    newNames=[]
+    oldNames=[]
+    oldDates=[]
+    newDates=[]
     for n in names:
         try:
             temp=pd.read_csv(f'stocks/data/{n}.csv')
-            df.iat[0,1]=temp.Date[-1]
+            if temp is not None:
+                date=temp['Date'].iloc[-1]
+                oldNames.append(n)
+                #TODO Transform date into DateTime
+                oldDates.append(date)
         except:
-            print(2)
-            df.iat[0, 1]=None
+            newNames.append(n)
+            newDates.append(datetime(2014, 1, 1))
 
-    df.to_csv('stocks/names.csv', index=False)
+    #update so threading
+    percent=0.25
+    arr1=newNames[:int(len(newNames)*percent)]
+    arr2=newNames[int(len(newNames)*percent):int(len(newNames)*(percent*2))]
+    arr3=newNames[int(len(newNames)*percent*2):int(len(newNames)*(percent*3))]
+    arr4=newNames[int(len(newNames)*percent*3):]
+    # arr5=newNames[int(len(newNames)*percent*4):]
+    darr1 = newDates[:int(len(newDates) * percent)]
+    darr2 = newDates[int(len(newDates) * percent):int(len(newDates) * (percent * 2))]
+    darr3 = newDates[int(len(newDates) * percent * 2):int(len(newDates) * (percent * 3))]
+    darr4 = newDates[int(len(newDates) * percent * 3):int(len(newDates) * (percent * 4))]
+    # darr5 = newDates[int(len(newDates) * percent * 4):]
+    threads=[]
+    threads.append(threading.Thread(target=update, args=(darr1,arr1,url)))
+    threads.append(threading.Thread(target=update, args=(darr2,arr2,url)))
+    threads.append(threading.Thread(target=update, args=(darr3,arr3,url)))
+    threads.append(threading.Thread(target=update, args=(darr4,arr4,url)))
+    # threads.append(threading.Thread(target=update, args=(darr5,arr5,url)))
+    # update(darr1,arr1,url)
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+    print("FINISHED")
+    #filter3
 
-    pass
 
 #UPDATE TILL TODAY
-def filter3(url):
-    df = pd.read_csv('stocks/names.csv')
-    driver = webdriver.Chrome()
-    driver.get(url)
-    succ=0
-    fail=0
+def filter3(names,dates):
+
+    percent=0.2
+
+    thread=threading.Thread(target=update,args=())
     # Iterate through each name in the DataFrame
-    for n in df.Names:
 
-        Input = driver.find_element(By.ID, 'Code')
-        # Input.clear()  # Clear previous input
-        Input.send_keys(n)
-        btn = driver.find_element(By.CLASS_NAME, 'btn-primary-sm')
-        btn.click()
-
-
-
-        table = driver.find_element(By.ID, 'results')  # Adjust this locator to match your table
-        # ActionChains(driver).move_to_element(table).click().perform()
-
-        # time.sleep(200)
-
-        # Wait for the table rows to load after clicking the button
-        try:
-            # Wait until the expected table rows are present
-            WebDriverWait(driver, 100).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'tr')))
-
-            # Re-fetch the elements after waiting
-            elements = table.find_elements(By.TAG_NAME, 'tr')[2:] # Skip header rows
-            # Iterate through the rows and extract cell text
-            for e in elements:
-                temp=e.get_attribute('innerHTML')
-                soup=bs(temp, 'html.parser')
-                date=soup.find('td')
-                # Re-fetch cells for the current row
-                # print(date.text)
-                # cells = e.find_elements(By.TAG_NAME, 'td')
-                # if cells:  # Ensure there are cells to avoid index errors
-                #     print(cells[0].text)
-            succ+=1
-            print(f'Succesfully {n}')
-        except Exception as e:
-            print(f"An error occurred while processing {n}:")
-            fail+=1
-        time.sleep(0.5)
-    print(succ)
-    print(fail)
 
 
     pass
 
 
 
-def update(url, date_from, date_to,names):
+def update(dates,names,url):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
-    for name in names:
+    date_to = datetime.today()
+    for name,date in zip(names,dates):
         interval = timedelta(days=365)
         Input = driver.find_element(By.ID, 'Code')
         Input.send_keys(name)
         # Iterate through the dates in 365-day intervals
-        current_date = date_from
+        current_date = date
         new_list=[]
         while current_date < date_to:
             end_date=current_date + interval
@@ -143,7 +135,6 @@ def update(url, date_from, date_to,names):
             btn.click()
             # TO DO
             WebDriverWait(driver, 100).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'tr')))
-
             time.sleep(0.5)
             table = driver.find_element(By.CSS_SELECTOR, '#resultsTable > tbody:nth-child(2)')
             soup = BeautifulSoup(table.get_attribute('innerHTML'), 'html.parser')
@@ -173,27 +164,11 @@ def update(url, date_from, date_to,names):
                 new_list.append(stock)
 
             current_date = end_date
-            print(f'finished {current_date}')
         df=pd.DataFrame(new_list)
+        print(f"FINISHED with {name}")
         df.to_csv(f'stocks/data/{name}.csv',index=False)
-
-    #TODO
-    # nameDF=pd.read_csv(f'stocks/names.csv')
-    # nameDF.iat[index,1]=date_to
 
 
 
 url='https://www.mse.mk/mk/stats/symbolhistory/ALK'
-# driver = webdriver.Chrome()
-# driver.get(url)
-
-# filter1(driver)
-# filter2()
-# filter3(url)
-# temp(url)
-# driver.quit()
-start_date = datetime(2014, 1, 1)
-currentday=datetime.today()
-names=['ADIN','BANA','BGOR']
-update(url, start_date, currentday,names)
-
+filter1(url)
